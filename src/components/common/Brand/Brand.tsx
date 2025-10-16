814ac36e557bb69c6df602e0e2e80bf0c65647d6
Mousam Kalita <mousamdreamer@gmail.com>
2025-10-16 23:54:55 +0530

    update brand logo

--- src/components/common/Brand/Brand.tsx
+++ src/components/common/Brand/Brand.tsx
@@ -4,10 +4,7 @@ import styles from "./Brand.module.css";
 export default function Brand() {
 	return (
 		<div className={styles.wrap} aria-label="SparkSafe">
-			<span className={styles.logo} aria-hidden>
-				{/* Simple fire glyph */}
-				<span style={{ color: "var(--color-danger)" }}>🔥</span>
-			</span>
+			<img className={styles.logo} src="/brand_logo.png" alt="SparkSafe logo" />
 			<span className={styles.name}>SparkSafe</span>
 		</div>
 	);
